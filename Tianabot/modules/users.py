eabdd4a50cb558714c6933e3acb0592f4e608212
Khiladi King <79006624+Kishoth-45@users.noreply.github.com>
2021-07-29 06:25:25 -0700

    Update users.py

--- Tianabot/modules/users.py
+++ Tianabot/modules/users.py
@@ -74,17 +74,27 @@ def broadcast(update: Update, context: CallbackContext):
         if to_group:
             for chat in chats:
                 try:
-                   context.bot.sendMessage(int(chat.chat_id), to_send[1])
-                sleep(0.1)
-            except TelegramError:
-                failed_group += 1
+                   context.bot.sendMessage(
+                        int(chat.chat_id),
+                        to_send[1],
+                        parse_mode="MARKDOWN",
+                        disable_web_page_preview=False,
+                    )
+                    sleep(0.1)
+                except TelegramError:
+                    failed += 1
         if to_user:
             for user in users:
                 try:
-                    context.bot.sendMessage(int(chat.chat_id), to_send[1])
-                sleep(0.1)
-            except TelegramError:
-                failed_user += 1
+                    context.bot.sendMessage(
+                        int(user.user_id),
+                        to_send[1],
+                        parse_mode="MARKDOWN",
+                        disable_web_page_preview=False,
+                    )
+                    sleep(0.1)
+                except TelegramError:
+                    failed_user += 1
         update.effective_message.reply_text(
             f"Broadcast complete.\nGroups failed: {failed}.\nUsers failed: {failed_user}."
         )
