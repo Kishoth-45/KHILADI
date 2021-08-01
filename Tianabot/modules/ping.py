892cc919c6efe4b16d3dbb569ae495a48dbcc906
Khiladi King <79006624+Kishoth-45@users.noreply.github.com>
2021-08-01 00:18:42 -0700

    Update ping.py

--- Tianabot/modules/ping.py
+++ Tianabot/modules/ping.py
@@ -81,7 +81,7 @@ def ping(update: Update, context: CallbackContext):
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        TIANA_IMG "🥊PONG!!\n"
+        "TIANA_IMG" "🥊PONG!!\n"
         "<b>⏰Time Taken:</b> <code>{}</code>\n"
         "<b>🔮Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
