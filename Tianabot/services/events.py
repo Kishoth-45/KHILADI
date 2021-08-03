70407df01f7e322611d6dd6845e072730694f160
Khiladi King <79006624+Kishoth-45@users.noreply.github.com>
2021-08-03 00:36:50 -0700

    Update events.py

--- Tianabot/services/events.py
+++ Tianabot/services/events.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 from telethon import events
 
-from Tianabot.services.mongo import mongodb as db
+from Tianabot.helper_extra.mongo import mongodb as db
 from Tianabot.services.telethon import tbot
 
 gbanned = db.gban
