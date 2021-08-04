4b9525124a1f842d59877a2a6fb386c3a2366aaa
Khiladi King <79006624+Kishoth-45@users.noreply.github.com>
2021-08-04 03:21:48 -0700

    Update Spam.py

--- Tianabot/modules/Spam.py
+++ Tianabot/modules/Spam.py
@@ -4,16 +4,8 @@ from asyncio import wait
 
 from Tianabot.events import register
 
-@register(outgoing=True, pattern="^/tspam")
-async def tmeme(e):
-    tspam = str(e.text[7:])
-    message = tspam.replace(" ", "")
-    for letter in message:
-        await e.respond(letter)
-    await e.delete()
-
-@register(outgoing=True, pattern="^/spam")
-async def spammer(e):
+@run_async
+def spam(update: Update, context: CallbackContext):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         message = e.text
         counter = int(message[6:8])
@@ -21,9 +13,19 @@ async def spammer(e):
         await asyncio.wait([e.respond(spam_message) for i in range(counter)])
         await e.delete()
         if LOGGER:
-            await e.client.send_message(
+            await e.bot.send_message(
                 LOGGER_GROUP,
                 "#SPAM \n\n"
                 "Spam was executed successfully"
                 )
                                
+SPAM_HANDLER = DisableAbleCommandHandler("spam", spam)
+
+dispatcher.add_handler(SPAM_HANDLER)
+
+__command_list__ = [
+    "spam",
+]
+__handlers__ = [
+    SPAM_HANDLER
+]
