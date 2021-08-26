e273e290fb294212009332a6327e6d6da85c71e5
Khiladi King <79006624+Kishoth-45@users.noreply.github.com>
2021-08-25 23:43:42 -0700

    Update userinfo.py

--- Tianabot/modules/userinfo.py
+++ Tianabot/modules/userinfo.py
@@ -422,7 +422,16 @@ def set_about_me(update: Update, context: CallbackContext):
 @run_async
 @dev_plus
 def stats(update: Update, context: CallbackContext):
-    stats = "<b>📊 Current stats:</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
+    process = subprocess.Popen(
+        "neofetch --stdout", shell=True, text=True, stdout=subprocess.PIPE
+    )
+    output = process.communicate()[0]
+    stats = (
+        "<b>Current stats:</b>\n"
+        + "\n"
+        + output
+        + "\n".join([mod.__stats__() for mod in STATS])
+    )
     result = re.sub(r"(\d+)", r"<code>\1</code>", stats)
     update.effective_message.reply_text(result, parse_mode=ParseMode.HTML)
 
