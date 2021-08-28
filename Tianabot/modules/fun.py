a415bbfa2a19a1e9e6f8e71a075d9cd2384731e6
Khiladi King <79006624+Kishoth-45@users.noreply.github.com>
2021-08-27 22:47:52 -0700

    Update fun.py

--- Tianabot/modules/fun.py
+++ Tianabot/modules/fun.py
@@ -214,6 +214,13 @@ def cringe(update: Update, context: CallbackContext):
     reply_animation = update.effective_message.reply_to_message.reply_animation if update.effective_message.reply_to_message else update.effective_message.reply_animation
     reply_animation(random.choice(fun_strings.CRINGE))
     
+    
+@run_async
+def svideo(update: Update, context: CallbackContext):
+    reply_video = update.effective_message.reply_to_message.reply_video if update.effective_message.reply_to_message else update.effective_message.reply_video
+    reply_video(random.choice(fun_strings.VIDEO))
+    
+    
 @run_async
 def table(update: Update, context: CallbackContext):
     reply_text = update.effective_message.reply_to_message.reply_text if update.effective_message.reply_to_message else update.effective_message.reply_text
@@ -283,6 +290,7 @@ __help__ = """
 SANITIZE_HANDLER = DisableAbleCommandHandler("sanitize", sanitize)
 RUNS_HANDLER = DisableAbleCommandHandler("runs", runs)
 CRINGE_HANDLER = DisableAbleCommandHandler("cringe", cringe)
+SVIDEO_HANDLER = DisableAbleCommandHandler("svideo", svideo)
 SLAP_HANDLER = DisableAbleCommandHandler("slap", slap)
 PAT_HANDLER = DisableAbleCommandHandler("pat", pat)
 ROLL_HANDLER = DisableAbleCommandHandler("roll", roll)
@@ -298,6 +306,7 @@ WEEBIFY_HANDLER = DisableAbleCommandHandler("weebify", weebify)
 dispatcher.add_handler(SANITIZE_HANDLER)
 dispatcher.add_handler(RUNS_HANDLER)
 dispatcher.add_handler(CRINGE_HANDLER)
+dispatcher.add_handler(SVIDEO_HANDLER)
 dispatcher.add_handler(SLAP_HANDLER)
 dispatcher.add_handler(PAT_HANDLER)
 dispatcher.add_handler(ROLL_HANDLER)
@@ -312,11 +321,11 @@ dispatcher.add_handler(WEEBIFY_HANDLER)
 
 __mod_name__ = "FUN"
 __command_list__ = [
-    "runs","cringe", "slap", "roll", "toss", "shrug", "bluetext", "rlg", "decide",
+    "runs", "cringe", "svideo", "slap", "roll", "toss", "shrug", "bluetext", "rlg", "decide",
     "table", "pat", "sanitize", "weebify",
 ]
 __handlers__ = [
-    RUNS_HANDLER, CRINGE_HANDLER, SLAP_HANDLER, PAT_HANDLER, ROLL_HANDLER, TOSS_HANDLER,
+    RUNS_HANDLER, CRINGE_HANDLER, SVIDEO_HANDLER, SLAP_HANDLER, PAT_HANDLER, ROLL_HANDLER, TOSS_HANDLER,
     SHRUG_HANDLER, BLUETEXT_HANDLER, RLG_HANDLER, DECIDE_HANDLER, TABLE_HANDLER,
     SANITIZE_HANDLER, EIGHTBALL_HANDLER, WEEBIFY_HANDLER
 ]
