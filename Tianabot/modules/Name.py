0afb9fda33cfb930acc42ac936f843b0f895de57
Khiladi King <79006624+Kishoth-45@users.noreply.github.com>
2021-09-01 03:20:51 -0700

    Update Name.py

--- Tianabot/modules/Name.py
+++ Tianabot/modules/Name.py
@@ -5,34 +5,6 @@ from telethon.tl import functions, types
 from Tianabot.events import register as Tianabot
 
 
-async def is_register_admin(chat, user):
-
-    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
-
-        return isinstance(
-            (
-                await tbot(functions.channels.GetParticipantRequest(chat, user))
-            ).participant,
-            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
-        )
-    if isinstance(chat, types.InputPeerChat):
-
-        ui = await tbot.get_peer_id(user)
-        ps = (
-            await tbot(functions.messages.GetFullChatRequest(chat.chat_id))
-        ).full_chat.participants.participants
-        return isinstance(
-            next((p for p in ps if p.user_id == ui), None),
-            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
-        )
-    return None
-
-
-async def silently_send_message(conv, text):
-    await conv.send_message(text)
-    response = await conv.get_response()
-    await conv.mark_read(message=response)
-    return response
 
 
 @Tianabot(pattern="^/namehistory ?(.*)")
