11bd3f3b9df14970c40d686fe1f65e73e9eedee8
Khiladi King <79006624+Kishoth-45@users.noreply.github.com>
2021-09-01 05:53:32 -0700

    Update Name.py

--- Tianabot/modules/Name.py
+++ Tianabot/modules/Name.py
@@ -3,6 +3,7 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl import functions, types
 
 from Tianabot.events import register as Tianabot
+from Tianabot.services.telethon import tbot
 
 
 async def is_register_admin(chat, user):
@@ -36,66 +37,6 @@ async def silently_send_message(conv, text):
 
 
 @Daisy(pattern="^/namehistory ?(.*)")
-async def _(event):
-
-    if event.fwd_from:
-
-        return
-
-    if event.is_group:
-        if await is_register_admin(event.input_chat, event.message.sender_id):
-            pass
-        else:
-            return
-    if not event.reply_to_msg_id:
-
-        await event.reply("```Reply to any user message.```")
-
-        return
-
-    reply_message = await event.get_reply_message()
-
-    if not reply_message.text:
-
-        await event.reply("```reply to text message```")
-
-        return
-
-    chat = "@DetectiveInfoBot"
-    uid = reply_message.sender_id
-    reply_message.sender
-
-    if reply_message.sender.bot:
-
-        await event.edit("```Reply to actual users message.```")
-
-        return
-
-    lol = await event.reply("```Processing```")
-
-    async with ubot.conversation(chat) as conv:
-
-        try:
-
-            # response = conv.wait_event(
-            #   events.NewMessage(incoming=True, from_users=1706537835)
-            # )
-
-            await silently_send_message(conv, f"/detect_id {uid}")
-
-            # response = await response
-            responses = await silently_send_message(conv, f"/detect_id {uid}")
-        except YouBlockedUserError:
-
-            await event.reply("```Please unblock @DetectiveInfoBot and try again```")
-
-            return
-        await lol.edit(f"{responses.text}")
-        # await lol.edit(f"{response.message.message}")
- 
-
-
-@Tianabot(pattern="^/namehistory ?(.*)")
 async def _(event):
 
     if event.fwd_from:
