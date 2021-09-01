9f247234f807d166db03aca53f3cd0142d1f73d7
Khiladi King <79006624+Kishoth-45@users.noreply.github.com>
2021-09-01 05:44:29 -0700

    Update Name.py

--- Tianabot/modules/Name.py
+++ Tianabot/modules/Name.py
@@ -5,28 +5,94 @@ from telethon.tl import functions, types
 from Tianabot.events import register as Tianabot
 
 
-def user_admin(func):
-    @wraps(func)
-    def is_admin(update: Update, context: CallbackContext, *args, **kwargs):
-        bot = context.bot
-        user = update.effective_user
-        chat = update.effective_chat
+async def is_register_admin(chat, user):
 
-        if user and is_user_admin(chat, user.id):
-            return func(update, context, *args, **kwargs)
-        elif not user:
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+
+        return isinstance(
+            (
+                await tbot(functions.channels.GetParticipantRequest(chat, user))
+            ).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
+        )
+    if isinstance(chat, types.InputPeerChat):
+
+        ui = await tbot.get_peer_id(user)
+        ps = (
+            await tbot(functions.messages.GetFullChatRequest(chat.chat_id))
+        ).full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
+        )
+    return None
+
+
+async def silently_send_message(conv, text):
+    await conv.send_message(text)
+    response = await conv.get_response()
+    await conv.mark_read(message=response)
+    return response
+
+
+@Daisy(pattern="^/namehistory ?(.*)")
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    if event.is_group:
+        if await is_register_admin(event.input_chat, event.message.sender_id):
             pass
-        elif DEL_CMDS and " " not in update.effective_message.text:
-            try:
-                update.effective_message.delete()
-            except:
-                pass
         else:
-            update.effective_message.reply_text(
-                "Who dis non-admin telling me what to do? You want a punch?"
-            )
+            return
+    if not event.reply_to_msg_id:
 
-    return is_admin
+        await event.reply("```Reply to any user message.```")
+
+        return
+
+    reply_message = await event.get_reply_message()
+
+    if not reply_message.text:
+
+        await event.reply("```reply to text message```")
+
+        return
+
+    chat = "@DetectiveInfoBot"
+    uid = reply_message.sender_id
+    reply_message.sender
+
+    if reply_message.sender.bot:
+
+        await event.edit("```Reply to actual users message.```")
+
+        return
+
+    lol = await event.reply("```Processing```")
+
+    async with ubot.conversation(chat) as conv:
+
+        try:
+
+            # response = conv.wait_event(
+            #   events.NewMessage(incoming=True, from_users=1706537835)
+            # )
+
+            await silently_send_message(conv, f"/detect_id {uid}")
+
+            # response = await response
+            responses = await silently_send_message(conv, f"/detect_id {uid}")
+        except YouBlockedUserError:
+
+            await event.reply("```Please unblock @DetectiveInfoBot and try again```")
+
+            return
+        await lol.edit(f"{responses.text}")
+        # await lol.edit(f"{response.message.message}")
+ 
 
 
 @Tianabot(pattern="^/namehistory ?(.*)")
@@ -37,7 +103,7 @@ async def _(event):
         return
 
     if event.is_group:
-        if await user_admin(event.input_chat, event.message.sender_id):
+        if await is_register_admin(event.input_chat, event.message.sender_id):
             pass
         else:
             return
