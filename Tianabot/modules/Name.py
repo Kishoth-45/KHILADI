9b7006b70b7abb87e0a02eb8d191cdc4606c32b5
Khiladi King <79006624+Kishoth-45@users.noreply.github.com>
2021-09-01 05:31:33 -0700

    Update Name.py

--- Tianabot/modules/Name.py
+++ Tianabot/modules/Name.py
@@ -5,6 +5,30 @@ from telethon.tl import functions, types
 from Tianabot.events import register as Tianabot
 
 
+def user_admin(func):
+    @wraps(func)
+    def is_admin(update: Update, context: CallbackContext, *args, **kwargs):
+        bot = context.bot
+        user = update.effective_user
+        chat = update.effective_chat
+
+        if user and is_user_admin(chat, user.id):
+            return func(update, context, *args, **kwargs)
+        elif not user:
+            pass
+        elif DEL_CMDS and " " not in update.effective_message.text:
+            try:
+                update.effective_message.delete()
+            except:
+                pass
+        else:
+            update.effective_message.reply_text(
+                "Who dis non-admin telling me what to do? You want a punch?"
+            )
+
+    return is_admin
+
+
 @Tianabot(pattern="^/namehistory ?(.*)")
 async def _(event):
 
