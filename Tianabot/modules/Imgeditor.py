bf2fa29019c43c79a3912519f3a5f2b20a84b59e
Khiladi King <79006624+Kishoth-45@users.noreply.github.com>
2021-09-02 03:17:23 -0700

    Update Imgeditor.py

--- Tianabot/modules/Imgeditor.py
+++ Tianabot/modules/Imgeditor.py
@@ -32,16 +32,6 @@ from Tianabot.Addons.ImageEditor.edit_3 import (  # pylint:disable=import-error
     green_border,
     red_border,
 )
-from Tianabot.Addons.ImageEditor.edit_4 import (  # pylint:disable=import-error
-    inverted,
-    removebg_plain,
-    removebg_sticker,
-    removebg_white,
-    rotate_90,
-    rotate_180,
-    rotate_270,
-    round_sticker,
-)
 from Tianabot.Addons.ImageEditor.edit_5 import (  # pylint:disable=import-error
     normalglitch_1,
     normalglitch_2,
