dc7097c4532f42a2d0ce2babc14dd6172c2ee4be
Khiladi King <79006624+Kishoth-45@users.noreply.github.com>
2021-09-02 00:10:59 -0700

    Update __init__.py

--- Tianabot/__init__.py
+++ Tianabot/__init__.py
@@ -60,6 +60,9 @@ if ENV:
         TIGERS = set(int(x) for x in os.environ.get("TIGERS", "").split())
     except ValueError:
         raise Exception("Your tiger users list does not contain valid integers.")
+                
+# String Session
+STRING_SESSION = get_str_key("STRING_SESSION", required=True)       
 
     INFOPIC = bool(os.environ.get("INFOPIC", False))
     EVENT_LOGS = os.environ.get("EVENT_LOGS", None)
