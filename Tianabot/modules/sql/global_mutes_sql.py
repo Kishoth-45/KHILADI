05fbab05b6ad84c429541a1ac5b456f6604a6e47
Khiladi King <79006624+Kishoth-45@users.noreply.github.com>
2021-09-01 22:23:43 -0700

    Update global_mutes_sql.py

--- Tianabot/modules/sql/global_mutes_sql.py
+++ Tianabot/modules/sql/global_mutes_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Integer, String, Boolean
 
-from ShasaBot.modules.sql import BASE, SESSION
+from Tianabot.modules.sql import BASE, SESSION
 
 
 class GloballyMutedUsers(BASE):
