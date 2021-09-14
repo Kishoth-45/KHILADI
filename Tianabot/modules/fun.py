1d382ff10d78775776649115ba5a312bc13ff9b6
Khiladi King <79006624+Kishoth-45@users.noreply.github.com>
2021-09-14 06:03:12 -0700

    Update fun.py

--- Tianabot/modules/fun.py
+++ Tianabot/modules/fun.py
@@ -182,7 +182,7 @@ def toss(update: Update, context: CallbackContext):
 @run_async
 def ran(update: Update, context: CallbackContext):
     update.message.reply_video(
-       "BAACAgQAAx0CX1IIhgACA_lhQJDYlYAqtRCgdXkp9tEEPDZtEQACRg0AAm1F8FE9HbSDy4wT7CAE", caption= "Ranged Tamil movie 700mb \n Uploaded By Anjel \n If you want More prints ask @Movie_Request2"
+       "BAACAgQAAx0CX1IIhgACA_xhQJ1fv7ympPf8IV_yCLXyG_KMsAACdgoAAuaDWVHwsJnbVEoRPCAE", caption= "Ranged Tamil movie 700mb \n Uploaded By Anjel \n If you want More prints ask @Movie_Request2"
     )
 
 @run_async
