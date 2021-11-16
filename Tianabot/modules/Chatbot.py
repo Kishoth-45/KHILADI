0c62a429ef845188e6dc1c91ecad58702f164df1
Khiladi King <79006624+Kishoth-45@users.noreply.github.com>
2021-11-16 05:40:06 -0800

    Update Chatbot.py

--- Tianabot/modules/Chatbot.py
+++ Tianabot/modules/Chatbot.py
@@ -3,7 +3,7 @@ import re
 import os
 import html
 import requests
-import TGNRobot.modules.sql.kuki_sql as sql
+import Tianabot.modules.sql.kuki_sql as sql
 
 from time import sleep
 from telegram import ParseMode
