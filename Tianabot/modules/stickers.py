bb62e5cc66b69a9ed9586c27694fa1147de5ed8a
Khiladi King <79006624+Kishoth-45@users.noreply.github.com>
2021-11-19 05:42:16 -0800

    Update stickers.py

--- Tianabot/modules/stickers.py
+++ Tianabot/modules/stickers.py
@@ -2,7 +2,7 @@ import os
 import math
 import requests
 import urllib.request as urllib
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 from html import escape
 from bs4 import BeautifulSoup as bs
 
